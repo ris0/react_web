6d6836736058105f5f5353eb34ca2fa9b9268539
unicodesnowman <utfsnowman@gmail.com>
2016-04-13 20:02:45 -0400

    fix prod publicPath

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -12,7 +12,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, '..', 'build'),
         filename: 'bundle.min.js',
-        publicPath: '/'
+        publicPath: '/assets'
     },
 
     module: {
