5668b7c75c9588d75dde03b5f99043f09f9e2d3b
unicodesnowman <utfsnowman@gmail.com>
2016-05-06 14:27:27 -0400

    remove g+ icon, align main nav links left

--- client/components/SocialButtons.js
+++ client/components/SocialButtons.js
@@ -7,7 +7,6 @@ function SocialButtons(props) {
             <a href="#"><span className="icon icon-facebook" /></a>
             <a href="#"><span className="icon icon-twitter" /></a>
             <a href="#"><span className="icon icon-pinterest" /></a>
-            <a href="#"><span className="icon icon-google" /></a>
             <a href="#"><span className="icon icon-youtube" /></a>
             <a href="#"><span className="icon icon-instagram_round" /></a>
         </div>
