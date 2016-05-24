6efcfbdac31c5df9d64df3ca1e6a6311016eaeef
unicodesnowman <utfsnowman@gmail.com>
2016-05-24 11:17:07 -0400

    bring back test dependency

--- test/client/components/MainNavigationSpec.js
+++ test/client/components/MainNavigationSpec.js
@@ -11,10 +11,10 @@ describe('MainNavigation', function() {
 
     it('should render all links', function() {
         const mainNavigation = shallow(<MainNavigation navLinks={['uno', 'dos', 'tres']} />)
-        const links = mainNavigation.find('a')
-        expect(links.at(0).text()).to.eql('uno')
-        expect(links.at(1).text()).to.eql('dos')
-        expect(links.at(2).text()).to.eql('tres')
+        const links = mainNavigation.find('Link')
+        expect(links.at(0).props().to).to.eql('/uno')
+        expect(links.at(1).props().to).to.eql('/dos')
+        expect(links.at(2).props().to).to.eql('/tres')
     });
 
     it('should render SocialButtons', function() {
